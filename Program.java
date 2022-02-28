713a44ab1e3403978060adea067dfe39469ab46c
Paul <cjr.paul@baypath.net>
2022-02-28 12:47:34 -0500

    changed sam

--- Program.java
+++ Program.java
@@ -2,7 +2,8 @@ public class Program {
 
     public static void main(String[] args) {
         System.out.println("Ayden Letourneau");
-        System.out.println("Sam");
+        System.out.println("Sam Boria");
+        System.out.println("Christian paul");
     }
     
 }
